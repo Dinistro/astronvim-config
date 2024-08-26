7847b3fc45de787662ab5ddb6f23e25d744eb2f1
Christian Ulmann <christian.ulmann@nextsilicon.com>
2024-08-26 15:08:54 +0000

    LSP: Add telescope mappings

--- lua/community.lua
+++ lua/community.lua
@@ -8,4 +8,5 @@ return {
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
   { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.recipes.telescope-lsp-mappings" },
 }
